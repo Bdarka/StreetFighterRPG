76a6498a3e02ed63c98b5b597a8dea9edd75714a
Bdarka <kslackie@inside.artcenter.edu>
2024-10-16 15:53:31 -0700

    Added Battle Manager and Time Manager Logic
    Gotta make this battle system work somehow amirite?

--- SF2RPG/Assets/Scripts/BattleManager.cs
+++ SF2RPG/Assets/Scripts/BattleManager.cs
@@ -13,9 +13,6 @@ using System;
 // Keeping track of whose turn it is. Trust me this helps with the ATB System
 public enum BattleState
 {
-    Start,
-    Win,
-    Lose,
     CharacterTurn1,
     CharacterTurn2,
     CharacterTurn3,
@@ -25,14 +22,18 @@ public enum BattleState
     EnemyTurn3,
     EnemyTurn4,
     EnemyTurn5,
-    EnemyTurn6
+    EnemyTurn6,
+    Start,
+    Win,
+    Lose
 }
 
 public class BattleManager : MonoBehaviour
-{
+{ 
     // Setup for Battle
     public BattleState battleState;
     public UIManager UIManager;
+    public TimeManager TimeManager;
 
     // Player Character Variables 
     public Transform[] playerPositions;
@@ -41,6 +42,9 @@ public class BattleManager : MonoBehaviour
     // Enemy Variables
     public Transform[] enemyPositions;
     public List <CharacterClass> enemyCharacters = new List<CharacterClass>();
+
+    // I just want a list with all battle characters for certain calculations
+    public List<CharacterClass> battleParticipants = new List<CharacterClass>();
 
     // Start is called before the first frame update
     void Start()
@@ -54,7 +58,6 @@ public class BattleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
     private void SetupBattle()
     {
@@ -69,7 +72,7 @@ public class BattleManager : MonoBehaviour
             }
 
             Instantiate(playerCharacters[i], playerPositions[i]);
-
+            battleParticipants.Add(playerCharacters[i]);
         }
 
         UIManager.SetupPlayerUI(playerCharacters);
@@ -83,17 +86,83 @@ public class BattleManager : MonoBehaviour
             }
 
             Instantiate(enemyCharacters[i], enemyPositions[i]);
-
+            battleParticipants[i] = enemyCharacters[i];
         }
 
         UIManager.SetEnemyUI(enemyCharacters);
+
+        TimeManager.battleParticipants = battleParticipants;
     }
 
+    public void WhoseTurn(CharacterClass character)
+    {
+        // Look I know this could've been prettier but I'm gonna be so real:
+        // This is gonna get shit done
+
+        // Basically we're counting down in the timer function, then passing over whichever character's timer reaches zero
+        // then setting the battle state to that character's turn. Should also avoid issues with out of range indexes.
+        int turn = battleParticipants.IndexOf(character);
+
+        switch (turn)
+        {
+            case 0:
+                {
+                    battleState = BattleState.CharacterTurn1;
+                    break;
+                }
+            case 1:
+                {
+                    battleState = BattleState.CharacterTurn2;
+                    break;
+                }
+            case 2:
+                {
+                    battleState = BattleState.CharacterTurn3;
+                    break;
+                }
+            case 3:
+                {
+                    battleState = BattleState.CharacterTurn4;
+                    break;
+                }
+            case 4:
+                {
+                    battleState = BattleState.EnemyTurn1;
+                    break;
+                }
+            case 5:
+                {
+                    battleState = BattleState.EnemyTurn2;
+                    break;
+                }
+            case 6:
+                {
+                    battleState = BattleState.EnemyTurn3;
+                    break;
+                }
+            case 7:
+                {
+                    battleState = BattleState.EnemyTurn4;
+                    break;
+                }
+            case 8:
+                {
+                    battleState = BattleState.EnemyTurn5;
+                    break;
+                }
+            case 9:
+                {
+                    battleState = BattleState.EnemyTurn6;
+                    break;
+                }
+        }
+    }
 
     public void CalculateDamage(int damage, string DamageType, int attackerStrength, int defenderFortitude)
     {
         //if(DamageType == Physical)
     }
+
 
     // Roll the rewards from the battle and then display it to the player
     public void BattleRewards()
---
+++ SF2RPG/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public BattleManager battleManager;
+    public List<CharacterClass> battleParticipants = new List<CharacterClass>();
+    public CharacterClass currentCharacterTurn;
+    public bool counting;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // you need this bool or its just gonna keep counting down even when the turn is being done
+
+        if ((counting == true))
+        {
+            foreach (CharacterClass c in battleParticipants)
+            {
+                if (c == null)
+                {
+                    break;
+                }
+
+                c.lastTurnRecovery -= Time.deltaTime;
+
+                if (c.lastTurnRecovery < 0)
+                {
+                    currentCharacterTurn = c;
+                    battleManager.WhoseTurn(c);
+                    counting = false;
+                }
+            }
+        }
+       
+    }
+}
